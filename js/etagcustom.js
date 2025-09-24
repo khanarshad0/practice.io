c0c911a06621995bd45963672b97c7246351e846
khanarshad0 <110396349+khanarshad0@users.noreply.github.com>
2025-09-24 12:23:52 +0530

    Update etagcustom.js

--- js/etagcustom.js
+++ js/etagcustom.js
@@ -1,10 +1,9 @@
-// Hide iframe
-document.getElementById("iframe2").style.display = "none";
+$("#iframe2").hide();
 
 function track() {
     var iframe = document.getElementById("iframe2");
     var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
     var body = innerDoc.body;
-    var etag = body.innerHTML;
-    
-    document.getElementById("trackingId").innerHTML = etag;
+	var etag = body.innerHTML;
+	$("#trackingId").html(etag);	
+}
